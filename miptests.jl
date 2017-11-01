5fa92badcabe2e71d94d34837fb40d71a1a41314
Andrew Rosemberg <andrewrosemberg@hotmail.com>
2017-11-01 12:21:40 -0200

    Teste3 adicionado. Se trata do problema da Expansao da Producao

--- miptests.jl
+++ miptests.jl
@@ -35,3 +35,29 @@ function test2(solveMIP::Function)
         # TODO testar conteudo da struct "sol"
     end
 end
+
+#teste problema 6 da lista (Expansao da Producao)
+#adicionado por Andrew Rosemberg
+function test3(solveMIP::Function)
+    Cinv = 13.16
+    M = 200
+    @testset "Teste da Expansao da Producao" begin
+        model = Model()
+        @variable(model, x[i=1:2]>=0)
+        @variable(model, u, Bin)
+        @objective(model, Max, 4*x[1] + 3*x[2] - u*Cinv)
+
+        @constraint(model, 2*x[1] + 1*x[2] <= 4 +u*M)
+        @constraint(model, 1*x[1] + 2*x[2] <= 4 +u*M)
+
+        @constraint(model, 1*x[1] + 0.1*x[2] <= 4 +(1-u)*M)
+        @constraint(model, 0.4*x[1] + 1*x[2] <= 4 +(1-u)*M)
+
+        sol = solveMIP(model)
+        @test getobjectivevalue(m) == 9.340000000000002
+        @test getvalue(x) == [3.75, 2.5]
+        @test getvalue(u) == 1
+
+        # TODO testar conteudo da struct "sol"
+    end
+end
