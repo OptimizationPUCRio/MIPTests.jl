b9df1d1d332497ceaaddcede9cb74c5fcd033be6
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-11-06 10:53:51 -0200

    Adding Robust n-K Unit Commitment test

--- miptests.jl
+++ miptests.jl
@@ -111,3 +111,81 @@ function testInfeasibleKnapsack(solveMIP::Function, solver::MathProgBase.Abstrac
         @test m.ext[:status] == :Infeasible
     end
 end
+
+
+# teste n-K robusto - trabalho da P1 - caso com 10 geradores e K = 2
+# adicionado por Raphael Saavedra
+function testRobustCCUC(solveMIP::Function, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
+  #------------------------------------------------------------------------------
+  # Parameters
+  T = collect(1:24) # periods
+  N = collect(1:10) # generators
+  p0 = zeros(10) # initial power output
+  v0 = zeros(10) # initial on/off state
+  D = 0.5*[700; 750; 850; 950; 1000; 1100; 1150; 1200; 1300; 1400; 1450; 1500; 1400;
+            1300; 1200; 1050; 1000; 1100; 1200; 1400; 1300; 1100; 900; 800] # demand
+  K = 2 # n-K security criterion
+  Cf = [1000; 970; 700; 680; 450; 370; 480; 660; 665; 670] # fixed cost
+  Cl = [16.19; 17.26; 16.6; 16.5; 19.7; 22.26; 27.74; 25.92; 27.27; 27.79] # linear cost
+  Cs = 0.08*Cl # spinning reserve cost
+  Cns = 0.1*Cl # non-spinning reserve cost
+  Pmax = [455; 455; 130; 130; 162; 80; 85; 55; 55; 55] # generator capacity
+  Pmin = [150; 150; 20; 20; 25; 20; 25; 10; 10; 10] # minimum power output
+  RSmax = Pmax # maximum spinning reserve
+  RNSmax = Pmax # maximum non-spinning reserve
+  RD = Pmax # ramp-down limit
+  RU = Pmax # ramp-up limit
+  SD = RD # shutdown ramp limit
+  SU = RU # startup ramp limit
+  #------------------------------------------------------------------------------
+  @testset "Teste Robust n-K Unit Commitment" begin
+    # Model formulation
+    m = Model(solver = solver)
+    #----------------------------------------------------------------------------
+    # Variables
+    @variable(m, p[1:N[end], 1:T[end]] >= 0) # power output
+    @variable(m, v[1:N[end], 1:T[end]], Bin) # 1 if generator is on, 0 otherwise
+    @variable(m, 0 <= rs[i = 1:N[end], 1:T[end]] <= RSmax[i]) # spinning reserve
+    @variable(m, rns[1:N[end], 1:T[end]] >= 0) # non-spinning reserve
+    @variable(m, vns[1:N[end], 1:T[end]], Bin) # 1 if generator provides non-spinning reserve, 0 otherwise
+    @variable(m, y[1:T[end]] >= 0) # dual variable of the n-K constraint
+    @variable(m, z[1:N[end], 1:T[end]] >= 0) # dual variable of the upper bound
+    #----------------------------------------------------------------------------
+    # Constraints
+    @constraint(m, [t in T], sum(p[i,t] for i in N) == D[t])
+    @constraint(m, [t in T, i in N], p[i,t] >= Pmin[i]*v[i,t])
+    @constraint(m, [t in T, i in N], p[i,t] <= Pmax[i]*v[i,t])
+    @constraint(m, [t in T, i in N], p[i,t] + rs[i,t] <= Pmax[i]*v[i,t])
+    @constraint(m, [t in T, i in N], rns[i,t] >= Pmin[i]*vns[i,t])
+    @constraint(m, [t in T, i in N], rns[i,t] <= RNSmax[i]*vns[i,t])
+    @constraint(m, [t in T, i in N], v[i,t] + vns[i,t] <= 1)
+    @constraint(m, [t in 2:T[end], i in N], p[i,t-1] <= p[i,t] + RD[i]*v[i,t] + SD[i]*(v[i,t-1]-v[i,t]) + Pmax[i]*(1-v[i,t-1]))
+    @constraint(m, [t in 2:T[end], i in N], p[i,t] <= p[i,t-1] + RU[i]*v[i,t-1] + SU[i]*(v[i,t]-v[i,t-1]) + Pmax[i]*(1-v[i,t]))
+    @constraint(m, [t in 2:T[end], i in N], p[i,t] + rs[i,t] <= p[i,t-1] + RU[i]*v[i,t-1] + SU[i]*(v[i,t]-v[i,t-1]) + Pmax[i]*(1-v[i,t]))
+    @constraint(m, [t in 2:T[end], i in N], rns[i,t] <= p[i,t-1] + RU[i]*v[i,t-1] + RNSmax[i]*(1-v[i,t-1]))
+    @constraint(m, [t in 2:T[end], i in N], rns[i,t] <= SU[i]*(vns[i,t]-v[i,t-1]) + RNSmax[i]*(1-(vns[i,t]-v[i,t-1])))
+    @constraint(m, [t in T], (N[end]-K)*y[t] - sum(z[i,t] for i in N) >= D[t])
+    @constraint(m, [t in T, i in N], y[t] - z[i,t] <= p[i,t] + rs[i,t] + rns[i,t])
+
+    # Constraints regarding initial state
+    @constraint(m, [t in 1, i in N], p0[i] <= p[i,t] + RD[i]*v[i,t] + SD[i]*(v0[i]-v[i,t]) + Pmax[i]*(1-v0[i]))
+    @constraint(m, [t in 1, i in N], p[i,t] <= p0[i] + RU[i]*v0[i] + SU[i]*(v[i,t]-v0[i]) + Pmax[i]*(1-v[i,t]))
+    @constraint(m, [t in 1, i in N], p[i,t] + rs[i,t] <= p0[i] + RU[i]*v0[i] + SU[i]*(v[i,t]-v0[i]) + Pmax[i]*(1-v[i,t]))
+    @constraint(m, [t in 1, i in N], rns[i,t] <= p0[i] + RU[i]*v0[i] + RNSmax[i]*(1-v0[i]))
+    @constraint(m, [t in 1, i in N], rns[i,t] <= SU[i]*(vns[i,t]-v0[i]) + RNSmax[i]*(1-(vns[i,t]-v0[i])))
+    #----------------------------------------------------------------------------
+    # Objective function
+    @objective(m, Min, sum(Cf[i]*v[i,t] for i in N, t in T) + sum(Cl[i]*p[i,t] for i in N, t in T) +
+                            sum(Cs[i]*rs[i,t] for i in N, t in T) + sum(Cns[i]*rns[i,t] for i in N, t in T))
+  #------------------------------------------------------------------------------
+    sol = solveMIP(m)
+
+    @test abs(getobjectivevalue(m) - 289892.9539) < 1e-3
+    @test getvalue(p[:,24]) == [400; zeros(9)]
+    @test getvalue(v[1,:]) == ones(24)
+    @test getvalue(v[:,1]) == [1; zeros(9)]
+    @test getvalue(v[:,4]) == [1; zeros(4); 1; zeros(4)]
+    @test sum(getvalue(v)) == 43
+
+  end
+end
