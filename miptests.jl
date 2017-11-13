dc2c7d71ee7eba54e3ea77ab44063c7660bd4b2e
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-11-13 19:28:17 -0200

    Adding infeasible UC test and unbounded MIP test

--- miptests.jl
+++ miptests.jl
@@ -424,3 +424,76 @@ function testCaminho(solveMIP::Function, solver::MathProgBase.AbstractMathProgSo
         @test sum(getvalue(x)) == 3
     end
 end
+
+function test_MIP_Unbounded_Raphael(solveMIP::Function, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
+    @testset "Teste Mochila unbounded" begin
+        m = Model(solver = solver)
+        @variable(m, x[i=1:3], Bin)
+        @variable(m, y >= 0)
+        @constraint(m, 6*x[1] + 5*x[2] + 5*x[3] <= 5)
+        @objective(m, Max, 6*x[1] + 4*x[2] + 3*x[3] + y)
+
+        sol = solveMIP(m)
+
+        @test m.ext[:status] == :Unbounded
+    end
+end
+
+function testUnboundedKnapsack(solveMIP::Function, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
+    @testset "Teste Mochila unbounded" begin
+        m = Model(solver = solver)
+        @variable(m, x[i=1:3], Bin)
+        @variable(m, y >= 0)
+        @constraint(m, 6*x[1] + 5*x[2] + 5*x[3] <= 5)
+        @objective(m, Max, 6*x[1] + 4*x[2] + 3*x[3] + y)
+
+        sol = solveMIP(m)
+
+        @test m.ext[:status] == :Unbounded
+    end
+end
+
+function testInfeasibleUC(solveMIP::Function, solver::MathProgBase.AbstractMathProgSolver = JuMP.UnsetSolver())
+  #------------------------------------------------------------------------------
+  # Parameters
+  T = collect(1:10) # periods
+  N = collect(1:5) # generators
+  p0 = zeros(N[end]) # initial power output
+  v0 = zeros(N[end]) # initial on/off state
+  D = [400; 390; 380; 370; 360; 350; 340; 330; 320; 600] # demand
+  Cf = [100; 100; 100; 100; 100] # fixed cost
+  Cl = [10; 20; 30; 40; 50] # linear cost
+  Pmax = [100; 100; 100; 100; 100] # generator capacity
+  Pmin = [10; 10; 10; 10; 10] # minimum power output
+  RD = [10; 20; 30; 40; 50] # ramp-down limit
+  RU = [10; 25; 30; 40; 50] # ramp-up limit
+  SD = RD # shutdown ramp limit
+  SU = RU # startup ramp limit
+  #------------------------------------------------------------------------------
+  @testset "Teste Infeasible Unit Commitment" begin
+
+    m = Model(solver = solver)
+    #----------------------------------------------------------------------------
+    # Variables
+    @variable(m, p[1:N[end], 1:T[end]] >= 0) # power output
+    @variable(m, v[1:N[end], 1:T[end]], Bin) # 1 if generator is on, 0 otherwise
+    #----------------------------------------------------------------------------
+    # Constraints
+    @constraint(m, [t in T], sum(p[i,t] for i in N) == D[t])
+    @constraint(m, [t in T, i in N], p[i,t] >= Pmin[i]*v[i,t])
+    @constraint(m, [t in T, i in N], p[i,t] <= Pmax[i]*v[i,t])
+    @constraint(m, [t in 2:T[end], i in N], p[i,t-1] <= p[i,t] + RD[i]*v[i,t] + SD[i]*(v[i,t-1]-v[i,t]) + Pmax[i]*(1-v[i,t-1]))
+    @constraint(m, [t in 2:T[end], i in N], p[i,t] <= p[i,t-1] + RU[i]*v[i,t-1] + SU[i]*(v[i,t]-v[i,t-1]) + Pmax[i]*(1-v[i,t]))
+    # Constraints regarding initial state
+    @constraint(m, [t in 1, i in N], p0[i] <= p[i,t] + RD[i]*v[i,t] + SD[i]*(v0[i]-v[i,t]) + Pmax[i]*(1-v0[i]))
+    @constraint(m, [t in 1, i in N], p[i,t] <= p0[i] + RU[i]*v0[i] + SU[i]*(v[i,t]-v0[i]) + Pmax[i]*(1-v[i,t]))
+    #----------------------------------------------------------------------------
+    # Objective function
+    @objective(m, Min, sum(Cf[i]*v[i,t] for i in N, t in T) + sum(Cl[i]*p[i,t] for i in N, t in T))
+  #------------------------------------------------------------------------------
+    sol = solveMIP(m)
+
+    @test m.ext[:status] == :Infeasible
+
+  end
+end
